7854b902d72ed3db1e2c20ebfc04890e6f9390ce
dominic bernardo <dominicvbern@gmail.com>
2022-12-18 23:46:22 -0800

    details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,33 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Game
 
 
-class Game:
-    def __init__(self, name, genre, description, year):
-        self.name = name
-        self.genre = genre
-        self.description = description
-        self.year = year
+# class Game:
+#     def __init__(self, name, genre, description, year):
+#         self.name = name
+#         self.genre = genre
+#         self.description = description
+#         self.year = year
 
 
-games = [
-    Game("Fantasy", "Genshin", "mobile game", 2020),
-    Game("FPS", "MW2", "War game", 2022),
-    Game("RPG", "Final Fantasy", "Story Game", 1990),
-]
+# games = [
+#     Game("Fantasy", "Genshin", "mobile game", 2020),
+#     Game("FPS", "MW2", "War game", 2022),
+#     Game("RPG", "Final Fantasy", "Story Game", 1990),
+# ]
 
 
 # Create your views here.
 # Add the following import
 def home(request):
     return render(request, "home.html")
-
-
 def about(request):
     return render(request, "about.html")
-
-    # Add new view
-
-
+# Add new view
 def games_index(request):
+    games = Game.objects.all()
     return render(request, "games/index.html", {"games": games})
